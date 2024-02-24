5b5117064773b7c532905ba3a5735136c7d503dd
Bryan Murphy <bryanmurphy02@gmail.com>
2024-02-24 13:49:56 -0500

    created base project

---
+++ mindful-moments/mindful-moments/mindful_momentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  mindful_momentsApp.swift
+//  mindful-moments
+//
+//  Created by Bryan Murphy on 2/24/24.
+//
+
+import SwiftUI
+
+@main
+struct mindful_momentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
