2e17cc151965d3a077d5d7ee7aa00df69e7ed918
Bryan Murphy <bryanmurphy02@gmail.com>
2024-03-07 13:54:02 -0500

    started settings page
    Main navigation view created and populated by two different types of settings:
    -Destination setting which brings the user to a new page
    -Toggle setting that allows the user to toggle the setting on and off

---
+++ mindful-moments/mindful-moments/NotificationsView.swift
@@ -0,0 +1,18 @@
+//
+//  NotificationsView.swift
+//  mindful-moments
+//
+//  Created by Bryan Murphy on 3/7/24.
+//
+
+import SwiftUI
+
+struct NotificationsView: View {
+    var body: some View {
+        Text("Bryan is cool!")
+    }
+}
+
+#Preview {
+    NotificationsView()
+}
--- mindful-moments/mindful-moments/SettingsView.swift
+++ mindful-moments/mindful-moments/SettingsView.swift
@@ -8,8 +8,74 @@
 import SwiftUI
 
 struct SettingsView: View {
+    
+    //state variables for toggle
+    @State var passwordLock = true
+    @State var aiGeneration = true
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            //this HStack will be for the back button in the top left
+//            HStack{
+//                Button(action: {
+//    //                    put action here
+//                }) {
+//                    Image(systemName: "arrow.backward")
+//                        .padding(.trailing, 300.0)
+//                        
+//                }
+//            }
+//            Spacer()
+            NavigationView{
+                List {
+                    Section(header: Text("General")) {
+                        SettingsToggleRow(icon: "lock.fill", title: "Password Lock", isToggled: $passwordLock)
+                        SettingsDestinationRow(icon: "bell.fill", title: "Notifications", destination: NotificationsView())
+                        SettingsToggleRow(icon: "brain.filled.head.profile", title: "AI Generation", isToggled: $aiGeneration)
+                    }
+                }
+                .listStyle(GroupedListStyle())
+                .navigationTitle("Settings")
+            }
+        }
+    }
+}
+
+//This struct sets it up to bring the user to a new page when clicking on the item
+struct SettingsDestinationRow<Destination: View>: View {
+    var icon: String //icon
+    var title: String //title of setting
+    var destination: Destination //page to be directed to
+    
+    var body: some View {
+        NavigationLink(destination: destination){
+            HStack{
+                Image(systemName: icon)
+                    .foregroundColor(.blue)
+                    .font(.headline)
+                Text(title)
+            }
+        }
+    }
+}
+
+//This struct is set up to be a toggle for the setting
+struct SettingsToggleRow: View {
+    var icon: String
+    var title: String
+    @Binding var isToggled: Bool
+    
+    var body: some View {
+        HStack {
+            Image(systemName: icon)
+                .foregroundColor(.blue)
+                .font(.headline)
+            Text(title)
+            Spacer()
+            Toggle("", isOn: $isToggled)
+                .labelsHidden()
+        }
     }
 }
 
