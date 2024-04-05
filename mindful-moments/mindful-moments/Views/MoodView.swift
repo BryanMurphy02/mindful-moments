247fb6d7d4d28cbebd8cf07f53f917fd2c7cb5be
Bryan Murphy <bryanmurphy02@gmail.com>
2024-04-04 20:52:03 -0400

    starting to debug pi chart so branching

--- mindful-moments/mindful-moments/Views/MoodView.swift
+++ mindful-moments/mindful-moments/Views/MoodView.swift
@@ -46,48 +46,21 @@ import SwiftUI
 
 let textProcessor = textProcessing()
 
-struct EmotionData {
-    let emotions: [String: Int]
-}
+//struct EmotionData {
+//    let emotions: [String: Int]
+//}
 
 struct PieChartView: View {
-    let data: EmotionData
+    let data: [String:Int]
     
     var body: some View {
         VStack {
-            PieChart(data: data.emotions.map { $0.value }, labels: data.emotions.map { $0.key })
+            PieChart(data: data)
                 .frame(width: 300, height: 300)
         }
     }
 }
 
-//struct PieChart: View {
-//    let data: [Int]
-//    let labels: [String]
-//    let colors: [Color] = [.red, .green, .blue, .orange, .yellow, .purple, .pink, .cyan, .gray, .brown]
-//
-//    var body: some View {
-//        GeometryReader { geometry in
-//            ZStack {
-//                ForEach(0..<data.count, id: \.self) { index in
-//                    PieChartSlice(
-//                        startAngle: angle(for: index),
-//                        endAngle: angle(for: index + 1),
-//                        color: colors[index % colors.count]
-//                    )
-//                }
-//            }
-//        }
-//    }
-//    
-//    func angle(for index: Int) -> Angle {
-//        guard !data.isEmpty else { return .zero }
-//        
-//        let total = data.reduce(0, +)
-//        let percentage = Double(data[index]) / Double(total)
-//        return .degrees(percentage * 360)
-//    }
-//}
 
 struct PieChart: View {
     let data: [String: Int]
@@ -146,8 +119,23 @@ struct PieChartSlice: View {
     }
 }
 
-struct MoodView: View {
+//struct MoodView: View {
 //    let emotionData = textProcessor.runProcessExample()
+//
+//    var body: some View {
+//        VStack {
+//            Text("Emotion Distribution")
+//                .font(.title)
+//                .padding()
+//            
+//            PieChartView(data: emotionData)
+//                .padding()
+//        }
+//    }
+//}
+
+struct MoodView: View {
+    let emotionData = textProcessor.runProcessExample()
 
     var body: some View {
         VStack {
@@ -155,11 +143,16 @@ struct MoodView: View {
                 .font(.title)
                 .padding()
             
-            PieChartView(data: textProcessor.runProcessExample())
+            PieChartView(data: emotionData)
+                .padding()
+            
+            // Print emotionData to check its content
+            Text("\(emotionData)")
                 .padding()
         }
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
